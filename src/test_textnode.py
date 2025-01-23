02852335369882a02daaefa363e2c364a62cfe28
lucas-j-holder <lucas.j.holder@gmail.com>
2025-01-22 21:46:15 -0800

    Implemented split nodes delimiter function.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextType, TextNode
+from textnode import TextType, TextNode, split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -38,5 +38,118 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(italic.__repr__(), "TextNode(TEST, italic, None)")
         self.assertEqual(code.__repr__(), "TextNode(TEST, code, None)")
     
+class Test_split_nodes_delimiter(unittest.TestCase):
+    def test_code(self):
+        start_node = TextNode("This is `code here` a test node.", TextType.TEXT)
+
+        nodes = split_nodes_delimiter([start_node], "`", TextType.CODE)
+
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text_type, TextType.CODE)
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+        self.assertEqual(nodes[0].text, "This is ")
+        self.assertEqual(nodes[1].text, "code here")
+        self.assertEqual(nodes[2].text, " a test node.")
+    
+    def test_bold(self):
+        start_node = TextNode("This is **bold here** a test node.", TextType.TEXT)
+
+        nodes = split_nodes_delimiter([start_node], "**", TextType.BOLD)
+
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+        self.assertEqual(nodes[0].text, "This is ")
+        self.assertEqual(nodes[1].text, "bold here")
+        self.assertEqual(nodes[2].text, " a test node.")
+    
+    def test_italic(self):
+        start_node = TextNode("This is *italic here* a test node.", TextType.TEXT)
+
+        nodes = split_nodes_delimiter([start_node], "*", TextType.ITALIC)
+
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+        self.assertEqual(nodes[0].text, "This is ")
+        self.assertEqual(nodes[1].text, "italic here")
+        self.assertEqual(nodes[2].text, " a test node.")
+
+    def test_code_left(self):
+        start_node = TextNode("`This is code here` a test node.", TextType.TEXT)
+
+        nodes = split_nodes_delimiter([start_node], "`", TextType.CODE)
+
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[1].text_type, TextType.CODE)
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+        self.assertEqual(nodes[1].text, "This is code here")
+        self.assertEqual(nodes[2].text, " a test node.")
+    
+    def test_bold_left(self):
+        start_node = TextNode("**This is bold here** a test node.", TextType.TEXT)
+
+        nodes = split_nodes_delimiter([start_node], "**", TextType.BOLD)
+
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+        self.assertEqual(nodes[1].text, "This is bold here")
+        self.assertEqual(nodes[2].text, " a test node.")
+    
+    def test_italic_left(self):
+        start_node = TextNode("*This is italic here* a test node.", TextType.TEXT)
+
+        nodes = split_nodes_delimiter([start_node], "*", TextType.ITALIC)
+        print(nodes)
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+        self.assertEqual(nodes[1].text, "This is italic here")
+        self.assertEqual(nodes[2].text, " a test node.")
+    
+    def test_italic_right(self):
+        start_node = TextNode("This is italic here a *test node.*", TextType.TEXT)
+
+        nodes = split_nodes_delimiter([start_node], "*", TextType.ITALIC)
+        print(nodes)
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+        self.assertEqual(nodes[1].text, "test node.")
+        self.assertEqual(nodes[2].text, "")
+    
+    def test_italic_multiple(self):
+        start_node = TextNode("This *is* italic *here* a *test* node.", TextType.TEXT)
+
+        nodes = split_nodes_delimiter([start_node], "*", TextType.ITALIC)
+        self.assertEqual(len(nodes), 7)
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(nodes[3].text_type, TextType.ITALIC)
+        self.assertEqual(nodes[4].text_type, TextType.TEXT)
+        self.assertEqual(nodes[5].text_type, TextType.ITALIC)
+        self.assertEqual(nodes[6].text_type, TextType.TEXT)
+
+        self.assertEqual(nodes[0].text, "This ")
+        self.assertEqual(nodes[1].text, "is")
+        self.assertEqual(nodes[2].text, " italic ")
+        self.assertEqual(nodes[3].text, "here")
+        self.assertEqual(nodes[4].text, " a ")
+        self.assertEqual(nodes[5].text, "test")
+        self.assertEqual(nodes[6].text, " node.")
+
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -19,3 +19,18 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        text = node.text
+        split_text = text.split(delimiter)
+        if len(split_text) == 1:
+            new_nodes.append(node)
+        else:
+            for i in range(len(split_text)):
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(split_text[i], node.text_type, None))
+                else:
+                    new_nodes.append(TextNode(split_text[i], text_type, None))
+    return new_nodes
